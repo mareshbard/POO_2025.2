addcfee48b82e709a2ee1c7f436f1d10ebae5bb0
Leticia Gomes <125154278+mareshbard@users.noreply.github.com>
2025-09-24 09:47:52 -0300

    Create Q2.cpp

---
+++ cpp/Q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    float n1, n2, media;
+    cout <<"Digite o primeiro numero: " << endl;
+    cin >> n1;
+    cout <<"Digite o segundo numero: " << endl;
+    cin >> n2;
+    media = (n1+n2)/2;
+    cout <<"Media dos numeros: " << media << endl;
+
+    return 0;
+}
